5a0602f96b67f61084b41ec9c9933debed426149
Amina Javed <61292170+amjav@users.noreply.github.com>
2020-11-08 14:57:16 +0000

    1st Addition of classes

---
+++ Meeting_Scheduler_Prototype/Meeting_Scheduler_Prototype/Meeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meeting_Scheduler_Prototype
+{
+    class Meeting
+    {
+        public List<DateTime> DateHourRange = new List<DateTime>();
+        //contains a range of date hours in which the meeting could be
+
+        public List<Participant> MeetingAttendees = new List<Participant>();
+        //list of participants that can attend the meeting
+
+        public List<Participant> RequestAttend = new List<Participant>();
+    }
+}
---
+++ Meeting_Scheduler_Prototype/Meeting_Scheduler_Prototype/Schedule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meeting_Scheduler_Prototype
+{
+    class Schedule
+    {
+        public List<DateTime> schedule = new List<DateTime>();
+        public List<DateTime> preference = new List<DateTime>();
+        public List<DateTime> exclusion = new List<DateTime>();
+
+        public void AddMeeting(DateTime date)
+        {
+            
+        }
+
+        public bool attend(DateTime date)
+        {
+            //some sort of if statement placed here
+            return true;
+        }
+
+        public void AddPreference()
+        {
+
+        }
+
+        public void AddExclusion()
+        {
+
+        }
+
+    }
+}
